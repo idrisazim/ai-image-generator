2dacf4e3f4f7c514f27511e4f1b13d4d42cd4b0f
idrisazim <edrisazim25@gmail.com>
2024-01-04 21:03:14 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import AiImageGenerator from "./Components/Ai-Image-Generator/AiImageGenerator";
+
+function App() {
+  return (
+    <div>
+      <AiImageGenerator />
+      {/* <input type="text" className="ai-input" />
+    <button onClick={()=>{imageGenerator()}}>Click Me</button>
+    <img src={image_url} alt="" />
+    <button><a href={image_url} download={image_url}>Download</a></button> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Ai-Image-Generator/AiImageGenerator.jsx
@@ -0,0 +1,59 @@
+import React, { useRef, useState } from "react";
+import "./AiImageGenerator.css";
+import default_image from '../Assets/default_image.svg'
+
+const AiImageGenerator = () => {
+
+  const [image_url, setImage_url] = useState("/");
+  const [loading, setLoading] = useState(false);
+  let inputRef = useRef(null);
+
+  const imageGenerator = async () => {
+    if(inputRef.current.value==="")
+    {
+        return 0;
+    }
+    setLoading(true);
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization:
+          `Bearer ${process.env.REACT_APP_OPENAI_KEY}`,
+          "User-Agent": "Chrome",
+        },
+        body: JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n: 1,
+          size: "512x512",
+        }),
+      }
+    );
+    let data = await response.json();
+    console.log(data);
+    let data_array = data.data;
+    setImage_url(data_array[0].url);
+    setLoading(false);
+  };
+
+  return (
+    <div className='ai-image-generator'>
+      <div className="header">AI image <span>generator</span></div>
+      <div className="img-loading">
+        <div className="image"><img src={image_url==="/"?default_image:image_url} alt="." /></div>
+        <div className="loading">
+            <div className={loading?"loading-bar-full":"loading-bar"}></div>
+            <div className={loading?"loading-text":"display-none"}>Loading.....</div>
+        </div>
+      </div>
+      <div className="search-box">
+        <input type="text" ref={inputRef} className="search-input" placeholder="Describe what you want to see" />
+        <div className="generate-btn" onClick={()=>{imageGenerator()}}>Generate</div>
+      </div>
+    </div>
+  )
+};
+
+export default AiImageGenerator;
